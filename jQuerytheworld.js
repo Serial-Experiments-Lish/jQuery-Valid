9ad5f1c3e12c203af92d9a78926bd26796991d7e
ShengtraneChooChoo <lishengashiluli@gmail.com>
2022-04-05 01:58:04 -0400

    Initial commitments

---
+++ jQuerytheworld.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    $("#submit").click(function(event){    //Click function for button
+        const name = $("#name");           //Variables for different inputs
+        const email = $("#email");
+        const phone = $("#phone");
+        const messageArea = $("#message-area");
+        const required = [name, email, phone];
+    
+        for(i=0; i < required.length; i++) {
+            if(required[i].val() === "") {
+                $("#message").addClass("warning").html("<em>Please Fill Out Required Fields</em>");
+                $("label").addClass("warning");
+            } else {
+                $("#message").removeClass("warning");
+                $("label").removeClass("warning");
+                }
+    };
+        if($("label") != $(".warning")) {
+            $("form").remove();
+            $("h2").html('<em>Thanks for your feedback!</em>');}
+        });
+})
